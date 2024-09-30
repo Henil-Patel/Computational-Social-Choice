e0b19a83d50ee9270f193b0aff5cc103995166af
Henil <henil.patel@hotmail.com>
2024-09-29 20:17:00 -0400

    problem 4 graph generator added

---
+++ hw2/p4.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def generate_graph(adjacency_matrix: list, G):
+    mapping = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g'}
+    for i in range(len(adjacency_matrix)):
+        for j in range(i + 1, len(adjacency_matrix[i])):
+            if adjacency_matrix[i][j] == 1:
+                G.add_edge(mapping[i], mapping[j])
+
+    pos = nx.circular_layout(G)
+    nx.draw(G, pos, with_labels = True, node_color = "skyblue", node_size = 2000, edge_color = "black", arrows = True, font_size = 15)
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    print(G.out_edges())
+    plt.show()
+
+if __name__ == "__main__":
+    adjacency_matrix = [
+        [0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 1],
+        [1, 0, 0, 1, 0, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0, 0]
+    ]
+
+    G = nx.DiGraph()
+    generate_graph(adjacency_matrix, G)
